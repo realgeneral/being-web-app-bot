87c371fbdcc8b866e897762085b84760ee1bb2a7
artemtret <artem.tretykov74@gmail.com>
2024-10-04 08:04:26 +0300

    start script

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 import asyncio
-from server.database import engine
+from server.database import engine, async_session  
 from server.models import Base
 from server.models import User, Language, TaskType, Task, Referral, WalletTransaction, Log, BotText   # Import all your models
 
@@ -7,6 +7,20 @@ async def recreate_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+    async with async_session() as session:
+        async with session.begin():
+            languages = [
+                Language(code='en', name='English'),
+                Language(code='ru', name='Russian'),
+            ]
+
+            session.add_all(languages)
+            initial_task_types = [
+                TaskType(id=1, name='Bot'),
+                TaskType(id=2, name='Subscribe to Channel'),
+            ]
+            session.add_all(initial_task_types)
+        await engine.dispose()
     await engine.dispose()
 
 if __name__ == '__main__':
