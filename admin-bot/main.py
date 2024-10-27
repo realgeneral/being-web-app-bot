45b176c9c2963e5db2103ec01174c57c4e5ab8f7
artemtret <artem.tretykov74@gmail.com>
2024-10-27 13:39:52 +0300

    telegram bot

--- admin-bot/main.py
+++ admin-bot/main.py
@@ -18,25 +18,36 @@ dp = Dispatcher(bot)
 # Команда для экспорта таблиц
 @dp.message_handler(commands=['export_tables'])
 async def cmd_export_tables(message: types.Message):
-    await export_tables_to_excel()
-    await message.reply("Таблицы успешно экспортированы в XLSX файлы.")
-    await bot.send_document(message.chat.id, open('users.xlsx', 'rb'))
-    await bot.send_document(message.chat.id, open('tasks.xlsx', 'rb'))
-    await bot.send_document(message.chat.id, open('task_statuses.xlsx', 'rb'))
+    try:
+        await export_tables_to_excel()
+        await message.reply("Таблицы успешно экспортированы в XLSX файлы.")
+        await bot.send_document(message.chat.id, open('users.xlsx', 'rb'))
+        await bot.send_document(message.chat.id, open('tasks.xlsx', 'rb'))
+        await bot.send_document(message.chat.id, open('task_statuses.xlsx', 'rb'))
+    except Exception as e:
+        await message.reply(f"Произошла ошибка при экспорте таблиц: {str(e)}")
+
+
 
 # Команда для получения статистики пользователей
 @dp.message_handler(commands=['stat'])
 async def cmd_user_stats(message: types.Message):
-    stats_user = await get_user_statistics()
-    stats_task = await get_task_statistics()
+    try:
 
-    response = (
-        f"Общее количество пользователей: {stats_user['total_users']}\n"
-        f"Присоединилось за последние сутки: {stats_user['recent_users']}"
-    )
-    for status, count in stats_task['task_statuses'].items():
-        response += f"Количество задач со статусом '{status}': {count}\n"
-    await message.reply(response)
+        stats_user = await get_user_statistics()
+        stats_task = await get_task_statistics()
+
+        response = (
+            f"Общее количество пользователей: {stats_user['total_users']}\n"
+            f"Присоединилось за последние сутки: {stats_user['recent_users']}"
+        )
+        for status, count in stats_task['task_statuses'].items():
+            response += f"Количество задач со статусом '{status}': {count}\n"
+        await message.reply(response)
+    except Exception as e:
+        await message.reply(f"Произошла ошибка при получении статистики: {str(e)}")
+
+
 
 
 if __name__ == '__main__':
