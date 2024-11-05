f74db3be61f33a1d7e81d67b9353b53d55fd76a0
artemtret <artem.tretykov74@gmail.com>
2024-11-06 00:12:29 +0300

    upd

--- server/models/referrals.py
+++ server/models/referrals.py
@@ -9,11 +9,11 @@ class Referral(Base):
     id = Column(Integer, primary_key=True, index=True)
     referrer_id = Column(BigInteger, ForeignKey('users.id'), nullable=False)
     referred_id = Column(BigInteger, ForeignKey('users.id'), nullable=False)
-    referre_at = Column(
+    referred_at = Column(  # Исправлено название поля
         TIMESTAMP,
         nullable=False,
         server_default=text('CURRENT_TIMESTAMP')
     )
 
     referrer = relationship("User", back_populates="referrals_record", foreign_keys=[referrer_id])
-    referred = relationship("User", foreign_keys=[referred_id])
+    referred = relationship("User", back_populates="referrals_received", foreign_keys=[referred_id])  # Добавлено back_populates
